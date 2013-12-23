168ce40e7b35fd705e376a2ef9449daa8783326b
Akaguara <akaguara@gmail.com>
2013-12-22 19:09:31 -0500

    Added additional fields to ActionBar

--- mitsloan2/src/com/example/mitsloan2/HomeScreen.java
+++ mitsloan2/src/com/example/mitsloan2/HomeScreen.java
@@ -1,10 +1,12 @@
 package com.example.mitsloan2;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class HomeScreen extends Activity {
+public class HomeScreen extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +18,31 @@ public class HomeScreen extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home_screen, menu);
+        MenuInflater inflator = getMenuInflater();
+        //inflator.inflate(R.menu.home_screen, menu);
+        inflator.inflate(R.menu.main_activity_actions, menu);     
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                openSearch();
+                return true;
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    public void openSearch(){
+    }
+    
+    public void openSettings(){
     }
     
 }
